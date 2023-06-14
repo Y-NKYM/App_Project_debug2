10047c70916b6887f09adf4b03d80f459992be55
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-14 01:36:01 +0000

    [Update] users/editまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      byebug
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render "edit"
